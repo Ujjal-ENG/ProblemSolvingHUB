515967f65b56ac4dc247ba186e3cb91531aa115f
Ujjal-ENG <ujjalroy7862@gmail.com>
2023-04-02 19:14:51 +0600

    solved this problem

--- 278Problem.js
+++ 278Problem.js
@@ -1,6 +1,12 @@
 var solution = function (isBadVersion) {
   return function (n) {
-    if (isBadVersion) return isBadVersion;
-    return n;
+    let left = 1;
+    let right = n;
+    while (left < right) {
+      let mid = Math.floor((left + bad) / 2);
+      if (isBadVersion(mid)) return (right = mid);
+      else return mid + 1;
+    }
+    return left;
   };
 };
