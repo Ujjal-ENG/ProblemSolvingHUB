3ad96dbb66ebeaf92b27490ead6477e5322fef79
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-02 21:39:15 +0600

    passed almost 330 test case but not solve yet

--- test.js
+++ test.js
@@ -1,21 +1,9 @@
-var letterCombinations = function (digits) {
-  let ht = {
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz",
-  };
-  for (let i of digits) {
-    for (let j in ht) {
-      if (i == j) {
-        console.log(i);
-      }
-    }
-  }
+var isPalindrome = function (s) {
+  const newStr = s.replace(/[\s,:,;,.]+/g, "");
+
+  return (
+    newStr.split("").reverse().join("").toLowerCase() === newStr.toLowerCase()
+  );
 };
 
-console.log(letterCombinations("23"));
+console.log(isPalindrome("a."));
